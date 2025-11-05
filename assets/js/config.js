1518c0f30b3b97d70663751044449c1f899eee3a
asuransiInternal <herman.him56@gmail.com>
2025-11-06 01:19:28 +0700

    Delete assets/js/config.js

--- assets/js/config.js
+++
@@ -1,3 +0,0 @@
-// assets/js/config.js
-// GANTI GAS_URL dengan URL Web App setelah deploy Apps Script
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxrWR_zLb5Uw256bgzXZm9oA8Mw3lhyOJRtPq_ZP9z7El6fb-OoDkgrMBvV_E4F0FpI/exec";

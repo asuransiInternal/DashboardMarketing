a4262731233243734a894250beb2f21938b5eca5
asuransiInternal <herman.him56@gmail.com>
2025-11-05 21:51:18 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+// assets/js/config.js
+// GANTI GAS_URL dengan URL Web App setelah deploy Apps Script
+const GAS_URL = "https://script.google.com/macros/s/AKfycbx7I3X-LH_TPAYH2bF_jv6SdQq4CmBzdhe3s11WVcf3v-sl3hqWgDA6SuUjlJJFHvs9/exec";

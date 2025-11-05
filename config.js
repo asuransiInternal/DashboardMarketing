c7de90b48a0c7e7d6e4310b5655767421ab57db1
asuransiInternal <herman.him56@gmail.com>
2025-11-05 22:07:25 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-const GAS_URL = "https://script.google.com/macros/s/AKfycbx7I3X-LH_TPAYH2bF_jv6SdQq4CmBzdhe3s11WVcf3v-sl3hqWgDA6SuUjlJJFHvs9/exec"; 
-// Ganti dengan URL deploy Web App kamu (Dari menu Deploy → Web App di Apps Script)
